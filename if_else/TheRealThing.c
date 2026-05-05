2e8d949f2271053d1982ffec311fbcd306a14b08
probable-acer <shubhongkar.codes@gmail.com>
2026-05-05 18:32:36 +0530

    if else

---
+++ if_else/TheRealThing.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int a;
+    if(3+2%5){
+        printf("This works\n");
+    }
+    if(a=10){
+        printf("Even this works\n");
+    }
+    if(-5){
+        printf("Suprisingly even this works\n");
+    }
+    if('a'){
+        printf("This works too!!");
+    }
+    return 0;
+}

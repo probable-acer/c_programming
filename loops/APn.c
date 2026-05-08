bb2b679a850e8ffdff270a171f271a68487f7b9c
probable-acer <shubhongkar.codes@gmail.com>
2026-05-08 20:50:36 +0530

    just small changes

--- loops/APn.c
+++ loops/APn.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
     int n;
-    printf("Enter a numner : ");
+    printf("Enter a number : ");
     scanf("%d",&n);
     for(int i = 4;i<=2*n-1;i=i+3){
         printf("%d ",i);

15275f55e03fa5125a19ae56fb9c292eb50d0950
probable-acer <shubhongkar.codes@gmail.com>
2026-05-09 23:12:03 +0530

    break and continue

---
+++ loops/primeNumber.c
@@ -0,0 +1,17 @@
+#include <stdio.h.>
+int main(){
+    int n;
+    printf("enter a number : ");
+    scanf("%d",&n);
+    int a = 0;
+    for(int i = 2;i<=n-1;i++){
+        if(n%i==0){ //i is a factor of n
+            a = 1;
+            break;
+        }
+    }
+    if(n==1) printf("1 is neither prime nor composite\n");
+    else if(a==0) printf("the given number is prime\n");
+    else printf("the given number is prime\n");
+    return 0;
+}

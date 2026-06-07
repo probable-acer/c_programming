1c818733cfe795f5af73a036cfcc6a64a96cc31d
Shubhongkar Ghosh <shubhongkar.codes@gmail.com>
2026-06-07 18:09:00 +0530

    Fix typo in prompt message for user input

--- loops/fibonaccinested.c
+++ loops/fibonaccinested.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
     int n;
-    printf("enter the mumber : ");
+    printf("enter the number : ");
     scanf("%d",&n);
     for(int i = 1;i<=n;i++){
         int a = 1;
